92adfee9310f240ec81a703b1bb189301da513dc
DevilOfThings <45946382+DevilOfThings@users.noreply.github.com>
2021-02-10 12:54:09 +0000

    Removed subquery for cleaness + performance

--- ConstructionLine.CodingChallenge.Tests/SearchEngineTestsBase.cs
+++ ConstructionLine.CodingChallenge.Tests/SearchEngineTestsBase.cs
@@ -37,11 +37,7 @@ namespace ConstructionLine.CodingChallenge.Tests
                 var sizeCount = sizeCounts.SingleOrDefault(s => s.Size.Id == size.Id);
                 // Is actually possible for size count to be null, if it's not added to the shirt list, can be demonstrated by setting the SampleDataBuilder to a low number
                 Assert.That(sizeCount, Is.Not.Null, $"Size count for '{size.Name}' not found in results");
-
-                var test = shirts
-                    .Select(s => s.Size.Id == size.Id
-                                && (searchOptions.Colors.Any() && searchOptions.Colors.Select(c => c.Id).Contains(s.Color.Id)));
-
+               
                 var expectedSizeCount = shirts
                     .Count(s => s.Size.Id == size.Id
                                 && (searchOptions.Colors.Any() && searchOptions.Colors.Select(c => c.Id).Contains(s.Color.Id)));
@@ -61,11 +57,7 @@ namespace ConstructionLine.CodingChallenge.Tests
                 var colorCount = colorCounts.SingleOrDefault(s => s.Color.Id == color.Id);
                 // Is actually possible for color count to be null, if it's not added to the shirt list, can be demonstrated by setting the SampleDataBuilder to a low number
                 Assert.That(colorCount, Is.Not.Null, $"Color count for '{color.Name}' not found in results");
-
-                var test = shirts
-                    .Select(c => c.Color.Id == color.Id
-                                && (searchOptions.Sizes.Any() && searchOptions.Sizes.Select(s => s.Id).Contains(c.Size.Id)));
-
+                
                 var expectedColorCount = shirts
                     .Count(c => c.Color.Id == color.Id  
                                 && (searchOptions.Sizes.Any() && searchOptions.Sizes.Select(s => s.Id).Contains(c.Size.Id)));
--- ConstructionLine.CodingChallenge/SearchEngine.cs
+++ ConstructionLine.CodingChallenge/SearchEngine.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace ConstructionLine.CodingChallenge
 {
     public class SearchEngine
     {
-        private readonly List<Shirt> _shirts;
-        private readonly HashSet<Shirt> hashShirts;
+        private readonly List<Shirt> _shirts;        
 
         public SearchEngine(List<Shirt> shirts)
         {
@@ -20,12 +20,27 @@ namespace ConstructionLine.CodingChallenge
         {
             var shirts = _shirts.SearchShirtColorsAndSizes(options.Colors, options.Sizes).ToList();
 
+            var tasks = new List<Task>();
+            var colors = Task.Run ( async () => await GetShirtColorCountsAsync(shirts, options));
+            var sizes = Task.Run( async () => await GetShirtSizeCountsAsync(shirts, options) );
+            Task.WhenAll(colors, sizes).ConfigureAwait(false).GetAwaiter();
+
             return new SearchResults
             {
-                ColorCounts = shirts.ShirtColorCounts(options.Colors).ToList(),
+                ColorCounts = colors.Result.ToList(),
                 Shirts = shirts,
-                SizeCounts = shirts.ShirtSizeCounts(options.Sizes).ToList()
+                SizeCounts = sizes.Result.ToList()
             };
+        }
+
+        private async Task<IEnumerable<ColorCount>> GetShirtColorCountsAsync(IReadOnlyCollection<Shirt> shirts, SearchOptions options)
+        {
+            return await Task.Run( () => shirts.ShirtColorCounts(options.Colors) );
+        }
+
+        private async Task<IEnumerable<SizeCount>> GetShirtSizeCountsAsync(IReadOnlyCollection<Shirt> shirts, SearchOptions options)
+        {
+            return await Task.Run( () => shirts.ShirtSizeCounts(options.Sizes) );
         }
     }
 }
